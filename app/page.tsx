57a33db7403753a06c3970b0db92c634f7940e61
LachlanRowley-AA <lachlan@assetalley.com.au>
2025-06-23 14:45:55 +1000

    Created privacy text as html

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { PrivacyFormText } from "@/lib/privacyFormText";
+
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: "white", minHeight: "100vh", padding: "1rem" }}>
+      <PrivacyFormText />
+    </div>
+  );
+}
